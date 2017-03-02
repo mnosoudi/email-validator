aa7ebe74de45b33f1493379ad9dc8c9df2c042b0
nosou <nosou@LAPTOP-OBJ4RS4Q>
2017-03-02 15:46:06 -0400

    2 new ruls added

--- email-validator/src/main/java/assignemnt_three_softwareEng/email_validator/emailValidator.java
+++ email-validator/src/main/java/assignemnt_three_softwareEng/email_validator/emailValidator.java
@@ -1,14 +1,56 @@
 package assignemnt_three_softwareEng.email_validator;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 /**
- * Hello world!
+ * method for checking invalid email inputs
  *
  */
-public class emailValidator 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        
-    }
-}
+public class emailValidator {
+	
+		public emailValidator(){
+			
+		}
+		int count = 0, count2=0;
+		
+		public  int validateThEmail(String email){
+			
+			if (email.contains(".")){
+				count++ ;
+			}
+				
+				
+				
+			for (int i=0; i<email.length(); i++){
+				
+				
+				if (email.charAt(i) == '@'){
+				count2++; 
+				
+					if(count2==1){
+					count++;
+					}
+				
+				}
+				
+			}	
+			
+			//String must contain only.com
+			if (email.contains(".com")){
+				 count++;
+			}
+			
+			//String must not be empty
+			if (!(email.length()==0)){
+				count++;
+			}
+			
+			
+			return count;
+			
+			
+		}
+	}
+
+
+
+
