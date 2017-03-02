3e91d047da7fdd950a098124f8fdb42981e150f1
nosou <nosou@LAPTOP-OBJ4RS4Q>
2017-03-01 21:38:47 -0400

    first stage

--- email-validator/src/main/java/assignemnt_three_softwareEng/email_validator/emailValidator.java
+++ email-validator/src/main/java/assignemnt_three_softwareEng/email_validator/emailValidator.java
@@ -1,14 +1,31 @@
 package assignemnt_three_softwareEng.email_validator;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 /**
- * Hello world!
+ * method for checking invalid email inputs
  *
  */
-public class emailValidator 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        
-    }
+public class emailValidator {
+
+
+	private Pattern pattern;
+	private Matcher matcher;
+
+	private static final String EMAIL_PATTERN =
+		"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@";
+
+	public emailValidator() {
+		pattern = Pattern.compile(EMAIL_PATTERN);
+	}
+
+	/**
+	 * validiate method set the hex and return the match
+	 */
+	public boolean validate(final String hex) {
+
+		matcher = pattern.matcher(hex);
+		return matcher.matches();
+
+	}
 }
+
