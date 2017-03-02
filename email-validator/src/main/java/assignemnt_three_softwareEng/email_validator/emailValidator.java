f35ea16d3b33e7159d30e2ea0f50a7672557351b
nosou <nosou@LAPTOP-OBJ4RS4Q>
2017-03-01 20:53:30 -0400

    empty class

---
+++ email-validator/src/main/java/assignemnt_three_softwareEng/email_validator/emailValidator.java
@@ -0,0 +1,14 @@
+package assignemnt_three_softwareEng.email_validator;
+
+/**
+ * Hello world!
+ *
+ */
+public class emailValidator 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
